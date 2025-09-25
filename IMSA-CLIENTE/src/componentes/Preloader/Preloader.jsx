cfdb24172fd3d75d8c7eb9e1d63f73c737768c10
Ale Candia <alejandrogcandia@gmail.com>
2025-09-25 10:27:48 -0300

    se agrega react-loader-spinner para el  preload, y se modifica el react v19  a v18 para que no tenga onflictos

--- IMSA-CLIENTE/src/componentes/Preloader/Preloader.jsx
+++ IMSA-CLIENTE/src/componentes/Preloader/Preloader.jsx
@@ -1,11 +1,22 @@
-import style from './Preloader.module.css'
-import logo from '../../assets/images/IMSA_Logo_Blanco.png'
-export default function Preloader(){
-    return(
-<div className={style.preloader}> 
-   
-<img src={logo} alt='Logo IMSA'/>
+import style from './Preloader.module.css';
+import logo from '../../assets/images/IMSA_Logo_Blanco.png';
+import {Hourglass} from 'react-loader-spinner'
 
-</div>
-    );
+export default function Preloader() {
+  return (
+    <div className={style.preloader}>
+      <img src={logo} alt="Logo IMSA" className={style.logo} />
+     <Hourglass
+  visible={true}
+  height="80"
+  width="80"
+  ariaLabel="hourglass-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
+  colors={[' #f4f0f9', '#372d69ff' ]}
+  />
+     
+    </div>
+  );
 }
+
