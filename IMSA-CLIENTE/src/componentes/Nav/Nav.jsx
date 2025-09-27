ad38c1e01c0b012c9253cf162e7889b4997e8875
Ale Candia <alejandrogcandia@gmail.com>
2025-09-27 13:04:54 -0300

    se trabaja en el menú hamburguesa

--- IMSA-CLIENTE/src/componentes/Nav/Nav.jsx
+++ IMSA-CLIENTE/src/componentes/Nav/Nav.jsx
@@ -3,8 +3,16 @@ import logo from '../../assets/images/logo_imsa_violeta_negro.png'
 import { IoCaretDown } from "react-icons/io5";
 import { FaFacebookF } from "react-icons/fa6";
 import { LuInstagram } from "react-icons/lu";
-import { FaUserCircle } from "react-icons/fa";
+import { CiMenuBurger } from "react-icons/ci";
+import { CiMenuFries } from "react-icons/ci";
+import { useState } from 'react'; // 👈 no olvides importar esto
+
 export default function Nav (){
+
+  
+// dentro del componente:
+const [menuAbierto, setMenuAbierto] = useState(false);
+
     return(
         <div className={style.nav}>
           <div className={style.logo}>
@@ -21,6 +29,27 @@ export default function Nav (){
             <FaFacebookF />
             <LuInstagram />
           </div>
+
+          {/* Menú Hamburguesa */}
+          <div className={style.hamburguesa} onClick={() => setMenuAbierto(!menuAbierto)}>
+  {menuAbierto ? <CiMenuFries size={28} style={{color:'#7a62fb' }} /> : <CiMenuBurger size={28} style={{color:'#7a62fb' }}/>}
+
+</div>
+{menuAbierto && (
+  <div className={style.menuMobile}>
+    <nav className={style.menuLinks}>
+      <a>CURSOS DE FORMACIÓN  <IoCaretDown /></a>
+      <a>NOSOTROS</a>
+      <a>CONTACTO</a>
+      
+    </nav>
+    <div className={style.menuRedes}>
+      <FaFacebookF />
+      <LuInstagram />
+    </div>
+  </div>
+)}
+
         </div>
     );
 }
