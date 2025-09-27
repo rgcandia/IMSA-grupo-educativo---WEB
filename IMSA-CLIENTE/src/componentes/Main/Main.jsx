2e549c4d2e95108f3cd324e378b2d47a28a7b693
Ale Candia <alejandrogcandia@gmail.com>
2025-09-27 17:04:49 -0300

    reload

--- IMSA-CLIENTE/src/componentes/Main/Main.jsx
+++ IMSA-CLIENTE/src/componentes/Main/Main.jsx
@@ -1,9 +1,9 @@
 // src/components/Main/Main.jsx
 import Carrousel from '../Carrousel/Carrousel';
-
+import style from './Main.module.css'
 export default function Main() {
   return (
-    <main>
+    <main className={style.main}>
       <Carrousel />
     </main>
   );
