da605616050c469b54b76fda718a0987c647cf3c
Ale Candia <alejandrogcandia@gmail.com>
2025-09-30 14:15:19 -0300

    reload

--- IMSA-CLIENTE/src/componentes/Formacion/Formacion.jsx
+++ IMSA-CLIENTE/src/componentes/Formacion/Formacion.jsx
@@ -2,37 +2,24 @@ import styles from './Formacion.module.css';
 
 export default function Formacion() {
   const areas = [
-    {
-      title: 'SALUD',
-      url_img: '/img/Formacion/salud.png',
-    },
-    {
-      title: 'IDIOMAS',
-      url_img: '/img/Formacion/idiomas.jpg',
-    },
-    {
-      title: 'BELLEZA',
-      url_img: '/img/Formacion/belleza.jpg',
-    },
-    {
-      title: 'DISEÑO',
-      url_img: '/img/Formacion/diseño.jpg',
-    },
-    {
-      title: 'TECNOLOGÍA',
-      url_img: '/img/Formacion/tecnologia.jpg',
-    },
-    {
-      title: 'TÉCNICOS',
-      url_img: '/img/Formacion/tecnicos.jpg',
-    },
+    { title: 'SALUD', url_img: '/img/Formacion/salud.png' },
+    { title: 'IDIOMAS', url_img: '/img/Formacion/idiomas.jpg' },
+    { title: 'BELLEZA', url_img: '/img/Formacion/belleza.jpg' },
+    { title: 'DISEÑO', url_img: '/img/Formacion/diseño.jpg' },
+    { title: 'TECNOLOGÍA', url_img: '/img/Formacion/tecnologia.jpg' },
+    { title: 'TÉCNICOS', url_img: '/img/Formacion/tecnicos.jpg' },
   ];
 
   return (
     <div className={styles.container}>
       {areas.map((area, index) => (
         <div key={index} className={styles.card}>
-          <img src={area.url_img} alt={area.title} className={styles.image} />
+          <img
+            src={area.url_img}
+            alt={area.title}
+            className={styles.image}
+            loading="eager" // <- fuerza a que el navegador la pinte cuanto antes
+          />
           <div className={styles.content}>
             <p className={styles.title}>{area.title}</p>
             <button className={styles.button}>Ver cursos</button>
